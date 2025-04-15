eb5e8419266154be7d213b2f1b89a658eb7c1fb7
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-04-15 20:11:11 +0530

    update: logo

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -237,12 +237,16 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <Image
-        src="https://assets.aceternity.com/logo-dark.png"
+        src="/logov4.png"
         alt="logo"
         width={30}
         height={30}
+        className="rounded-lg"
       />
-      <span className="font-medium text-black dark:text-white">Startup</span>
+      
+      <div className="font-sans">
+        <span className="text-xl font-bold tracking-tight bg-clip-text text-transparent bg-black">PrintMoney</span>
+      </div>
     </Link>
   );
 };
